c0c2fe6d77e4581eb5c21b15ae6aab4dc2274878
Noah Bakayou <NoahBakayou@gmail.com>
2025-09-16 11:39:10 -0700

    29.3

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Inscribed")]
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change directions
+    public float changeDirChance = 0.1f;
+
+    // Seconds between Apples instantiations
+    public float appleDropDelay = 1f;
+
     void Start()
     {
-        
+        // Start dropping apples
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Changing Direction
+        if (pos.x < -leftAndRightEdge) {           // a
+            speed = Mathf.Abs(speed);              // b
+        } else if (pos.x > leftAndRightEdge) {     // c
+            speed = -Mathf.Abs(speed);             //
+        }
     }
 }
